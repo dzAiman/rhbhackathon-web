33397277951710b4d2b62515b7d39419e0ebc061
mafiqqq <afiqqqx1997@gmail.com>
2021-06-12 00:41:45 +0800

    react init

---
+++ rhbhack-web/src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Container, ListGroup, Card, Button, Row, Col } from 'react-bootstrap'
+
+import  NavbarPage  from './components/NavbarPage/NavbarPage.js';
+function App() {
+  return (
+    <div className="App">
+      <NavbarPage />
+      <Container>
+        <Row>
+          <ListGroup>
+            <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          </ListGroup>
+        </Row>
+        <Row>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ rhbhack-web/src/components/NavbarPage/NavbarPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+const NavbarPage = () => {
+    return (
+      <Navbar>
+        <Navbar.Brand href="#home">RHB @ Workforce Gigs</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Dezairail</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
+
+export default NavbarPage;
+
